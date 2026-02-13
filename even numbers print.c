d88c54898f74a1721e86463a7c2536b92e5f383d
kavyasripalanisamy466-tech <kavyasripalanisamy466@gmail.com>
2026-02-13 16:10:29 +0530

    Add files via upload

---
+++ even numbers print.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    
+    for(int i=2;i<=10;i++){
+        
+         if(i%2==0){
+             
+             printf("%d ",i);
+         }
+    }
+    
+    
+    return 0;
+}
