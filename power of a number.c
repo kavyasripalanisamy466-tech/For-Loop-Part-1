bebaa515d16ac4ecba78e53a379c6999370207b1
kavyasripalanisamy466-tech <kavyasripalanisamy466@gmail.com>
2026-02-14 16:13:27 +0530

    Add files via upload

---
+++ power of a number.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int base,power;
+    int result=1;
+    
+    scanf("%d %d",&base,&power);
+    
+    for(int i=1;i<=power;i++){
+        
+        result=result*base;
+    }
+    printf("%d",result);
+    
+    return 0;
+}
